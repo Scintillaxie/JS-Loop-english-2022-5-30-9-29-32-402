9937dca332b6229ce8cbb0637a4a273c31c6d01f
Scintilla <2758257114@qq.com>
2022-06-07 14:07:43 +0800

    mul-table

--- multiplication_table/index.js
+++ multiplication_table/index.js
@@ -2,8 +2,7 @@ var i; //row
 var j; //column
 for (i = 1;i <= 9;i++){ //9rows
 	for(j = 1;j <= i;j++){ //j <= i 9column
-		//output the result
-		 document.write(j + "*" + i + "=" + i*j + "&nbsp;&nbsp;&nbsp;");
-	}
+	//output the result
+		document.write(j + "*" + i + "=" + i*j + "&nbsp;&nbsp;&nbsp;");	    }
 	document.write("<br/>"); // line feed
 }
