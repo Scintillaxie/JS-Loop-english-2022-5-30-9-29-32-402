01ace3030a43e1ebcec2bd40a6a75b6e52f62350
Scintilla <2758257114@qq.com>
2022-06-07 14:03:22 +0800

    odd or even number

--- number/index.js
+++ number/index.js
@@ -1,8 +1,8 @@
 for(var i = 1; i <= 20; i++){
 	if(i % 2 == 0){
-		console.log(i + ' is even number.');
+		document.write(i + ' is even number.');
 	}
 	else{
-		console.log(i+' is odd number.');
+		document.write(i+' is odd number.');
 	}
 }
